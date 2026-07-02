f382f301e0edcac3042fee96af15336fdd637ddc
Yidnekachew-SK <yidnekachewsk@gmail.com>
2026-07-02 15:26:03 +0300

    Finish the class component exercise
    Implement delete, edit and count mechanism for the displayed todo using class based component

--- class-components/src/components/ClassInput.jsx
+++ class-components/src/components/ClassInput.jsx
@@ -7,10 +7,15 @@ class ClassInput extends Component {
     this.state = {
       todos: ['Just some demo tasks', 'As an example'],
       inputVal: '',
+      count: 2,
+      isEdited: false,
+      clickCount: 0
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
   }
 
   handleInputChange(e) {
@@ -25,7 +30,34 @@ class ClassInput extends Component {
     this.setState((state) => ({
       todos: state.todos.concat(state.inputVal),
       inputVal: '',
+      count: state.count + 1,
     }));
+  }
+
+  handleDelete(todo) {
+    this.setState((state) => {
+      const newTodo = state.todos.filter((t) => t != todo)
+      return {...state, todos: newTodo, count: state.count - 1 } 
+    });
+  }
+
+  handleEdit(e, todo) {
+    if (this.state.clickCount === 0) {
+      e.target.textContent = "Resubmit";
+      this.setState((state) => ({...state, inputVal: todo, clickCount: 1}));
+      console.log(this.state.clickCount)
+    } else {
+      this.setState((state) => {
+        const newTodo = this.state.inputVal;
+        const modifiedTodos = state.todos.map(t => 
+          t === todo ? newTodo : t );
+
+        e.target.textContent = "Edit"
+
+        return {...state, todos: modifiedTodos, inputVal: '', clickCount: 0}
+      })
+    }
+     
   }
 
   render() {
@@ -47,9 +79,14 @@ class ClassInput extends Component {
         {/* The list of all the To-Do's, displayed */}
         <ul>
           {this.state.todos.map((todo) => (
-            <li key={todo}>{todo}</li>
+            <div key={`${todo}-container`}>
+              <li key={todo}>{todo}</li>
+              <button onClick={() => this.handleDelete(todo)}>Delete</button>
+              <button onClick={() => this.handleEdit(event,todo)}>Edit</button>
+            </div>
           ))}
         </ul>
+        <p>{this.state.count} todos</p>
       </section>
     );
   }
